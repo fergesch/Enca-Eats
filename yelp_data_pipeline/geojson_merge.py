d098426eb6b68122ff739d2ee769ccb2dec47e18
Michael Ferguson <mikeferg2011@gmail.com>
2023-11-25 09:50:08 -0500

    merge geojsons

---
+++ yelp_data_pipeline/geojson_merge.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import geopandas
+
+hob = geopandas.read_file('yelp_data_pipeline/geo_data/Hoboken_City_Limits.geojson')
+nyc = geopandas.read_file('yelp_data_pipeline/geo_data/nyc.geojson')
+jc = geopandas.read_file('yelp_data_pipeline/geo_data/jersey-city-neighborhoods.geojson')
+
+jc['city'] = 'Jersey City'
+jc['neighborhood'] = jc['area']
+
+jc_cond = jc['area'] == 'Downtown'
+jc.loc[jc['area'] == 'Downtown', 'neighborhood'] = jc.loc[jc_cond, 'area'] \
+    + ' - ' \
+    + jc.loc[jc_cond, 'neighborho']
+
+geo_list = [
+    hob[['NAME', 'GNIS_NAME', 'geometry']].rename(
+        {'NAME': 'neighborhood', 'GNIS_NAME': 'city'}, axis=1),
+    nyc[['NTAName', 'BoroName', 'geometry']].rename(
+        {'NTAName': 'neighborhood', 'BoroName': 'city'}, axis=1),
+    jc[['neighborhood', 'city', 'geometry']],
+]
+gdf = pd.concat(geo_list)
+gdf.to_file("yelp_data_pipeline/geo_data/uber.geojson", driver="GeoJSON")
